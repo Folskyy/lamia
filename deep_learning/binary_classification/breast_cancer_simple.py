a7b9e7b8c6e57305125de5351fdc98970ed17087
Gabriel Cruz <gabrielvieirahc@gmail.com>
2024-02-24 01:03:42 -0300

    simples implementação de um modelo de classificação binária capaz de determinar se um cancer é maligno ou não

--- deep_learning/binary_classification/breast_cancer_simple.py
+++ deep_learning/binary_classification/breast_cancer_simple.py
@@ -1,13 +1,13 @@
 import pandas as pd
-
+import numpy as np
 from sklearn.model_selection import train_test_split
 
 import keras
 from keras.models import Sequential
 from keras.layers import Dense
 
-previsores = pd.read_csv('/home/gabriel/Documentos/lamia/binary_classification/entradas_breast.csv')
-classe = pd.read_csv('/home/gabriel/Documentos/lamia/binary_classification/saidas_breast.csv')
+previsores = pd.read_csv('/home/gabriel/Documentos/lamia/deep_learning/binary_classification/entradas_breast.csv')
+classe = pd.read_csv('/home/gabriel/Documentos/lamia/deep_learning/binary_classification/saidas_breast.csv')
 
 # função automatica para fazer a divisao dos dados treinamento/teste
 previsores_treinamento, previsores_teste, classe_treinamento, classe_teste = train_test_split(previsores, classe, test_size=0.25)
@@ -24,7 +24,7 @@ classificador.add(Dense(units = 16, activation = 'relu', kernel_initializer = 'r
 classificador.add(Dense(units = 16, activation = 'relu', kernel_initializer = 'random_uniform'))
 classificador.add(Dense(units = 1, activation = 'sigmoid'))
 
-# keras.optimizers.Adam(): invoca um otimizador 'Adam' (algoritmo que iterativamente adapta a taxa de aprendizado)
+# keras.optimizers.Adam(): invoca um otimizador 'Adam' (algoritmo que iterativamente adapta a taxa de aprendizado).
 # lr: taxa de aprendizagem
 # decay: taxa de decaimento do learning_rate ao longo do treinamento
 # clipvalue: limitação do gradiente (x até -x)
@@ -32,10 +32,10 @@ otimizador = keras.optimizers.Adam(lr = 0.001, decay = 0.0001, clipvalue = 0.5)
 
 # ==========PARAMETROS==========
 # Sequential.compile(): configura o modelo para o treinamento
-# optimizer: Função para fazer o ajuste dos pesos Ex: SGD, adam, RMSprop...
+# optimizer: Função otimizadora (pode adaptar uma ou usar uma default)
 # loss: Função de perda(cálculo do erro) Ex: Mean Square Error, Mean Absolute Error... O binary_crossentropy é para classificações binárias
 # metrics: Medida(s) que voce deseja para avaliar o desempenho do modelo
-classificador.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['binary_accuracy'])
+classificador.compile(optimizer = otimizador, loss = 'binary_crossentropy', metrics = ['binary_accuracy'])
 
 # ==========PARAMETROS==========
 # Sequential.fit(): treina o modelo compilado no conjunto de dados fornecido.  "encaixa o modelo nos dados"
@@ -51,12 +51,23 @@ previsoes
 
 from sklearn.metrics import confusion_matrix, accuracy_score
 
+# Precisão dp modelo
 precisao = accuracy_score(classe_teste, previsoes)
+# matriz de confusão
 matriz = confusion_matrix(classe_teste, previsoes)
 
 classe_teste
 previsoes
 
+precisao
+matriz
+
+# recebe uma nparray com os pesos de uma das camadas ocultas
+# 16x16 da camada oculta + 1x16 do bias
+peso0 = np.array(classificador.layers[0].get_weights())
+peso1 = np.array(classificador.layers[1].get_weights())
 
 print(f"Número de acertos: {(previsoes == classe_teste).sum()}")
 print(f"Número de erros: {previsoes.size - (previsoes == classe_teste).sum()}")
+
+peso0
