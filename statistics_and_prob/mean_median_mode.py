a7c0c0d3cb47850bc067987e3456c7a631dab7e4
Gabriel Cruz <gabrielvieirahc@gmail.com>
2024-03-16 01:25:04 -0300

    moda, mediana e média

--- statistics_and_prob/mean_median_mode.py
+++ statistics_and_prob/mean_median_mode.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
-# ffigura 1
 plt.figure(1)
 renda = np.random.normal(27000, 15000, 10000) # distribuição gussiana: loc -> media da distrib; scale:-> desvio padrao; size -> formato da saída
 
@@ -12,7 +11,6 @@ np.median(renda) # mediana
 plt.hist(renda, 50)
 plt.show()
 
-# figura 2
 plt.figure(2)
 renda = np.append(renda, [1000000000]) # a função adiciona um nodo de 10⁷ na lista 'renda'
 
