7346dff0550de3e12280e2d0476864ed8f9c56b7
Gabriel Cruz <gabrielvieirahc@gmail.com>
2024-03-17 00:18:42 -0300

    simples implementação do método scraping

--- scraping/scraping_basics.py
+++ scraping/scraping_basics.py
@@ -1,16 +1,15 @@
 from bs4 import BeautifulSoup
-from colorama import Fore, Style
-import requests
+import requests # fazer requisições HTTP
 
-html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=Python&txtLocation=').text
+html_text = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=Python&txtLocation=').text # recebe o html do site em formato de texto
 
 html_text
 
-soup = BeautifulSoup(html_text, 'lxml')
+soup = BeautifulSoup(html_text, 'lxml') # trnsforma o texto html em um objeto
 
-jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx')
+jobs = soup.find_all('li', class_ = 'clearfix job-bx wht-shd-bx') # retorna todas as tags li com a classe clearfix job-bx wht-shd-bx
 
-job = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx')
+job = soup.find('li', class_ = 'clearfix job-bx wht-shd-bx') # retorna a primeira tag li com a classe clearfix job-bx wht-shd-bx
 
 company_name = job.find('h3', class_ = 'joblist-comp-name').text.replace(' ', '').replace('\n', '')
 company_name
